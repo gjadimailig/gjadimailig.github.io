4c3de8267cb569e3c5bcde93bed8181b9e130f49
AzureAD\GabrielJustinDIMAILI <dimailg@ph.ibm.com>
2018-05-10 08:51:35 -0700

    API.AI client development in js. Minor changes in css and html

--- scripts/port.js
+++ scripts/port.js
@@ -1,19 +1,67 @@
+var accessToken = "b81fb40e657e46b39200f3a102fb5244";
+var baseUrl = "https://api.api.ai/v1/";
+var randomSessionId = Math.floor(Math.random()*90000) + 10000;
+
 $(document).ready( function () {
-    scrollMessagesToBottom();
+    $(document).on("keypress", "#input-msg", function(e) {
+        if (e.which == 13) {
+            sendMessage();
+        }
+    });
+    sendAjaxRequestToAI("Hi!");
+    scrollMessagesToBottomLeft();
 });
+
+function appendMessageInChat(msg) {
+    var messageSeparator = ">";
+    var spaceSym = " ";
+    $("#messages-container").append('<div class=\"p-2 wrap\">'+ messageSeparator + spaceSym + msg +'</div>');
+    scrollMessagesToBottomLeft();
+}
+
+function clearInput(){
+    $('#input-msg').val("");
+    $('#input-msg').focus();
+}
+
+function scrollMessagesToBottomLeft() {
+    var objDiv = document.getElementById("messages-container");
+    objDiv.scrollTop = objDiv.scrollHeight;
+    objDiv.scrollLeft = 0;
+}
+
+function sendAjaxRequestToAI(msg) {
+    $.ajax({
+        type: "POST",
+        url: baseUrl + "query?v=20150910",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        headers: {
+            "Authorization": "Bearer " + accessToken
+        },
+        data: JSON.stringify({ query: msg, lang: "en", sessionId: randomSessionId }),
+        success: function(data) {
+            setResponse(data);
+            // setResponse(JSON.stringify(data, undefined, 2));
+        },
+        error: function() {
+            setResponse("Internal Server Error");
+        }
+    });
+}
 
 function sendMessage() {
     var msg = $('#input-msg').val();
-    if( !msg ) {
+    if( !msg ) 
         return;    
-    }
-    $("#messages-container").append('<div class=\"p-2\" >> '+ msg +'</div>');
-    scrollMessagesToBottom();
-    $('#input-msg').val("")
+
+    appendMessageInChat(msg);
+    clearInput();
+    sendAjaxRequestToAI(msg);
 }
 
-function scrollMessagesToBottom() {
-    var objDiv = document.getElementById("messages-container");
-    objDiv.scrollTop = objDiv.scrollHeight;
+function setResponse(data) {
+    appendMessageInChat(data.result.fulfillment.speech);   
 }
+
 
